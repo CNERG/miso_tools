918fb07827f700eb22ba3d8174de4940db94036b
Paul P.H. Wilson <wilsonp@engr.wisc.edu>
2013-11-21 15:46:08 -0600

    Added pseudo-code and sample data.
    Added pseudo-code to demonstrate possible ways to structure code to parse DA data.
    
    Added one set of sample DA data from 1/1/2012.

--- parseda.py
+++ parseda.py
@@ -37,5 +37,33 @@ with open('FILENAMEHERE.csv',rU) as g:
 	for row in reader:
 		if row[1] == 'WPS.KEWAUKEWA'
 			return row
-	
-	
+
+# method read 1 row of MISO Wide hourly data
+# def read_miso_wide_hourly_data
+#     load_csv_row
+#     hourly_data = csv_row[3:26]
+#     return hourly_data
+
+
+# def convert_daily_to_yearly(daily_data)
+#     for each date in daily_data
+#         for each hour daily_data[date]
+#             yearhour = date * 100 + hour
+#             year_data[yearhour] = daily_data[date][hour]
+
+# open file
+# skip first row
+# skip second row
+# read date from third row
+#      load_csv_row
+#      date = csv_row[0]
+#      convert date format (e.g. MM/DD/YYYY => YYYYMMDD)
+# skip rows 4,5
+# miso_wide[date] = read_miso_wide_hourly_data
+
+# when done, we have a data structure as follows:
+#
+# miso_wide[date] = array of 24 MCP values.
+#
+
+#
